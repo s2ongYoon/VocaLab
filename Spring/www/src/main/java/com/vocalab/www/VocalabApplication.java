cc9904a440c87b6c3a8df3db29f362221e2362a5
21V <futen1998@gmail.com>
2025-01-02 15:30:12 +0900

    Initial commit

---
+++ Spring/www/src/main/java/com/vocalab/www/VocalabApplication.java
@@ -0,0 +1,13 @@
+package com.vocalab.www;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VocalabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VocalabApplication.class, args);
+	}
+
+}
---
+++ Spring/www/src/test/java/com/vocalab/www/VocalabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vocalab.www;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VocalabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
