5be6c3af28e677c660ea0e683942aba39fbf76e3
21V <futen1998@gmail.com>
2025-01-07 15:01:28 +0900

    !HOTFIX Project Remake - Spring Security 오류로 프로젝트 재생성

---
+++ Spring/VocaLab/src/main/java/com/dev/vocalab/ServletInitializer.java
@@ -0,0 +1,12 @@
+package com.dev.vocalab;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(VocaLabApplication.class);
+    }
+}
---
+++ Spring/VocaLab/src/test/java/com/dev/vocalab/VocaLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.vocalab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VocaLabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
